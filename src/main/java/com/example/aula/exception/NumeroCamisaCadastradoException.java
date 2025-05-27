aa54170cd28592c02a4ca6ca93cedb37cfd5eb07
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2025-05-27 11:27:03 -0300

    Delete src directory

--- src/main/java/com/example/aula/exception/NumeroCamisaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class NumeroCamisaCadastradoException extends RuntimeException {
-    public NumeroCamisaCadastradoException(String message) {
-        super(message);
-    }
-}
